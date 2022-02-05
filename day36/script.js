1826dfe91ee423c916cc1cd41b69c1436e3e96fc
akshay <akshay.y@gmail.com>
2022-02-05 07:44:06 -0500

    day 36
    javascript and HTML counter app

---
+++ day36/script.js
@@ -0,0 +1,16 @@
+let counter = 0;
+
+function decrease() {
+    counter -= 1;
+    console.log(counter);
+}
+
+function increase() {
+    counter += 1;
+    console.log(counter);
+}
+
+function reset() {
+    counter = 0;
+    console.log(counter);
+}
