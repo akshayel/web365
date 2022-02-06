c8143ef6bfc80b0c6fad4a00214822ae50f8cbb7
akshay <akshay.y@gmail.com>
2022-02-06 18:51:06 -0500

    day 37
    JavaScript arrays

---
+++ day37/script.js
@@ -0,0 +1,13 @@
+//ARRAYS
+
+let colors = [];
+
+console.log(colors);
+
+let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+console.log(days);
+
+let mix = [12, 123.5, "cat", null, false, NaN, "dog"];
+
+console.log(mix);
