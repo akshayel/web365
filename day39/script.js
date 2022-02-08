98ee0f6cad403da5b317d5b1fa1c7338f1745fec
akshay <akshay.y@gmail.com>
2022-02-08 18:55:57 -0500

    day39
    JavaScript objects

---
+++ day39/script.js
@@ -0,0 +1,17 @@
+//OBJECTS
+
+const person = {
+    firstName: 'Bob',
+    lastName: 'Marley'
+};
+
+const ranVar = {
+    favNum: 123519231,
+    isFunny: true,
+    colors: ['red', 'orange']
+};
+
+const years = {
+    1999: "good",
+    2020: "bad"
+};
