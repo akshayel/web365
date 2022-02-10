3256fbfc03433b477dea445d5c1700d4910f7e6d
akshay <akshay.y@gmail.com>
2022-02-09 19:59:09 -0500

    day 40
    array and object combos in javascript

---
+++ day40/script.js
@@ -0,0 +1,34 @@
+const scores = {
+    george: 96,
+    gary: 77,
+    gertrude: 99
+};
+
+const shoppingCart = [
+    {
+        product: 'Milk',
+        price: 3.99,
+        qty: 44
+    },
+    {
+        product: 'Bread',
+        price: 2.49,
+        qty: 94
+    },
+    {
+        product: 'Honey',
+        price: 7.99,
+        qty: 3
+    }
+];
+
+const profile = {
+    username: 'lol123',
+    email: 'lol@aol.com',
+    tags: ['Cats', 'Dogs', 'Penguins'],
+    scores: {
+        assignments: 84,
+        midterm: 88,
+        final: 91
+    }
+};
