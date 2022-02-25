4823a486fc708b39257b065b35de759e4b73f6cf
akshay <akshay.y@gmail.com>
2022-02-25 06:08:01 -0500

    day 56
    defining methods using objects

---
+++ day56/app.js
@@ -0,0 +1,23 @@
+//DEFINING METHODS USING OBJECTS
+
+const myMath = {
+    PI: 3.14159,
+    add: function (x,y){
+        return x+y;
+    },
+    subtract: function(x,y) {
+        return x-y;
+    },
+    multiply: function(x,y){
+        return x*y;
+    },
+    divide: function(x,y){
+        return x/y;
+    },
+    square: function(x){
+        return x*x;
+    },
+    cube: function(x) {
+        return x**3;
+    }
+}
