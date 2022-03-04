95f0b189763955b22325a0ca086b48be3e94033f
Akshay Yeleswarapu <akshay.y@gmail.com>
2022-03-03 20:45:06 -0500

    day 61
    practising common string methods

---
+++ apptwo.js
@@ -0,0 +1,16 @@
+//JavaScript strings
+
+let example = "I like cats and dogs"
+
+//String indices
+
+example[5]; //e
+
+
+//String methods
+
+example.slice(5,9); //'e ca'
+example.substring(5,9); //'e ca'
+example.replace('cats', 'penguins'); //'I like penguins and dogs'
+
+example.concat(" lol"); //'I like cats and dogs lol'
